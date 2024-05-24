a480320ffdfed379e41f3af224d64e40d7753650
JustAlternate <lololollololol890@gmail.com>
2024-05-24 08:45:55 +0200

    abstract class for entity

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -3,15 +3,23 @@ package src.entities;
 import java.awt.*;
 import src.visuals.Geometry;
 
-public class Entity {
+/* We want this class to be abstract because we do not want people to instanciate raw Entity
+ * and for other reasons I found there :
+ * https://docs.oracle.com/javase/tutorial/java/IandI/abstract.html
+ */
+public abstract class Entity {
   public Geometry geometry;
 
-  public Entity(int x, int y, Geometry geometry) {
+  Entity(int x, int y, Geometry geometry) {
     this.geometry = geometry;
     geometry.rect.x = x;
     geometry.rect.y = y;
   }
 
+  /*
+   * This method should not be abstract because we want it to be the same for
+   * every entity.
+   */
   public void draw(Graphics2D g) {
     geometry.draw(g);
   }
